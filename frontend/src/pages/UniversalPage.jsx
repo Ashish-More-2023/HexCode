29e2ed389ad9c78ee2c117cb86152c23c3f2dbfe
ritvij611 <ritvijgopal6@gmail.com>
2025-01-31 01:19:42 +0530

    adds: made search bar functional

--- frontend/src/pages/UniversalPage.jsx
+++ frontend/src/pages/UniversalPage.jsx
@@ -1,39 +1,39 @@
 import { TemplateCard } from "../components/TemplateCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faRocket,faHexagonNodes } from "@fortawesome/free-solid-svg-icons";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 
-const templates = [
-    { title: "Blank Bot", description: "Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Website", description: "Crawl your website’s content to get answers to popular user questions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Zendesk", description: "Scan your Zendesk help center articles to answer customer questions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Knowledge Base", description: "Scrape your Knowledge Base articles to answer customer questions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Online Quiz Bot", description: "Engage your customers with a chatbot quiz tailored to your needs.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
-    { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" }
-  ];
+// const templates = [
+//     { title: "Blank Bot", description: "Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.Create a blank chatbot, which you can train and customize later.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Website", description: "Crawl your website’s content to get answers to popular user questions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Zendesk", description: "Scan your Zendesk help center articles to answer customer questions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Knowledge Base", description: "Scrape your Knowledge Base articles to answer customer questions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Online Quiz Bot", description: "Engage your customers with a chatbot quiz tailored to your needs.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Job Application Bot", description: "Automate sourcing candidates to speed up your hiring process.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "FAQ Bot", description: "Answer frequently asked questions with a chatbot and save time.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Customer Satisfaction", description: "Automate collecting surveys to capture your customers' voices and opinions.", imgSrc: "/assets/IMG_0664.jpg" },
+//     { title: "Create HelpDesk Tickets", description: "Let customers create HelpDesk tickets while chatting with your chatbot.", imgSrc: "/assets/IMG_0664.jpg" }
+//   ];
   
   const SearchBar = () => (
-    <div className="relative w-full max-w-sm my-3">
+    <form className="relative w-full max-w-sm my-3">
       <input
         className="w-full px-5 py-3 text-sm rounded-full bg-gray-800 border border-gray-600 text-white outline-none focus:ring-2 focus:ring-blue-500 transition"
         placeholder="Search projects..."
@@ -41,18 +41,51 @@ const templates = [
       <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-blue-500 transition">
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
-    </div>
+    </form>
   );
   
 export const UniversalPage = () => {
   const [hasMore, setHasMore] = useState(true);
   const [templates, setTemplates] = useState([]);
   const [visibleTemplates, setVisibleTemplates] = useState(templates.slice(0, 0));
+  const [searchTerm, setSearchTerm] = useState(""); // Search term for filtering users
+  const [searchResult, setSearchResult]= useState([]);
+  const [mount, setMount] = useState(false);
   
   const [error, setError] = useState(null);
+
+  const searchRef = useRef(null);
+
+  // Click outside handler
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setSearchResult([]); // Clear search results if clicked outside
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleSearch = ()=>{
+    console.log("searching....")
+    setSearchResult([]);
+    if(searchTerm.length===0)return;
+    visibleTemplates.map((template,index)=>{
+        const tosearch = template.name.trim().toLowerCase();
+        const search = searchTerm.trim().toLowerCase();
+        if(tosearch.includes(search)){
+            setSearchResult((old)=>[...old,template]);
+        }
+    });
+    console.log(searchResult);
+  }
   const getAllProjects = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/project/visible`, {
+      const response = await fetch(`http://localhost:3001/project/visible`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -66,18 +99,23 @@ export const UniversalPage = () => {
       }
 
       const data = await response.json();
+      // console.log(data);
       setTemplates(data); // Set the fetched projects
     } catch (err) {
       setError(err.message); // Capture and set the error
-      // console.log(error);
+      console.log(error);
     }
   };
   useEffect(()=>{
     getAllProjects();
   },[]);
   useEffect(()=>{
-    setVisibleTemplates(templates.slice(0, 0));
+    setVisibleTemplates(templates);
   },[templates]);
+  useEffect(()=>{
+    if(mount)handleSearch();
+    else setMount(true);
+  },[searchTerm])
   const [page, setPage] = useState(1);
 
   const loadMoreTemplates = () => {
@@ -95,13 +133,39 @@ export const UniversalPage = () => {
   };
 
   return (
-    <div className="min-h-screen text-white px-8 py-12">
+    <div className="min-h-screen w-[100%] text-white px-8 py-12">
       {/* Header */}
       <div className="flex flex-col md:flex-row justify-between items-center mb-6 ">
         <div>
           <h2 className="text-4xl font-semibold tracking-wide"><FontAwesomeIcon icon={faRocket} /> Public Projects</h2>
         </div>
-        <SearchBar />
+        {/* Searchbar */}
+        <div ref={searchRef} className="w-full md:w-[25rem]">
+        <div className="relative w-full max-w-lg mt-3 mb-1">
+          <input
+            onChange={(e)=>{setSearchTerm(e.target.value)}}
+            className="w-full px-5 py-3 text-sm rounded-full bg-gray-800 border border-gray-600 text-white outline-none focus:ring-2 focus:ring-blue-500 transition"
+            placeholder="Search projects..."
+            value={searchTerm}
+          />
+          <button onClick={()=>setSearchResult([])} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-blue-500 transition">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        {/* Search Results Dropdown */}
+        {searchResult.length > 0 && (
+            <div className="absolute bg-gray-800 border border-gray-600 rounded-lg shadow-lg z-10 w-[25rem]">
+              {searchResult.map((project, index) => (
+                <div 
+                  key={index}
+                  className="px-4 py-2 hover:bg-gray-700 cursor-pointer transition"
+                >
+                  {project.name}
+                </div>
+              ))}
+            </div>
+        )}
+        </div>
       </div>
 
        {/* Description */}
@@ -130,9 +194,9 @@ export const UniversalPage = () => {
               className="transform hover:scale-105 transition-all duration-300 shadow-md hover:shadow-xl rounded-xl overflow-hidden bg-gray-800 hover:bg-gray-700"
             >
               <TemplateCard 
-                id={index}
-                title={template.title} 
-                description={template.description}
+                id={template._id}
+                title={template.name} 
+                description={template.messages}
                 upVotes={0}
               />
             </div>
